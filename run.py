8cc5c3507d7643a924c166a3fe80ccf5e38fac68
ivanleoncz <ivanlmj@gmail.com>
2023-10-24 21:42:38 -0600

    feat: initial software and data
    Basic functions, helpers, datasets, and argument parsing for scripting
    execution via command line.

---
+++ run.py
@@ -0,0 +1,21 @@
+import argparse
+import json
+
+import pandas as pd
+
+from utils import prepare_dataframe_tri, prepare_dataframe_melate_retro, get_probability_of_numbers_per_column
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process and analyze datasets from Mexico Lottery services.")
+    parser.add_argument('--type', '-t', help="name of the lottery service", required=True,
+                        choices=['tris', 'melate_retro'], )
+    args = parser.parse_args()
+    if args.type and args.type == 'tris':
+        df = prepare_dataframe_tri(pd.read_csv('datasets/Tris.csv'))
+        del df['FECHA']
+        print(json.dumps(get_probability_of_numbers_per_column(df), indent=2, default=int))
+    if args.type and args.type == 'melate_retro':
+        df = prepare_dataframe_melate_retro(pd.read_csv('datasets/Melate-Retro.csv'))
+        del df['FECHA']
+        del df['BOLSA']
+        print(json.dumps(get_probability_of_numbers_per_column(df), indent=2, default=int))
---
+++ utils.py
@@ -0,0 +1,34 @@
+
+def prepare_dataframe_tri(df):
+    """
+    Returns filtered version, with columns of interest and data transformations (if necessary).
+    """
+    df.rename(columns={"R1": "C1", "R2": "C2", "R3": "C3", "R4": "C4", "R5": "C5", })
+    return df[['R1', 'R2', 'R3', 'R4', 'R5', 'FECHA']]
+
+
+def prepare_dataframe_melate_retro(df):
+    """
+    Returns filtered version, with columns of interest and data transformations (if necessary).
+    """
+    df.rename(columns={"F1": "C1", "F2": "C2", "F3": "C3", "F4": "C4", "F5": "C5", "F6": "C6", "F7": "C7", })
+    return df[['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'BOLSA', 'FECHA']]
+
+
+def filter_dataframe_by_year(df, year="2023"):
+    """
+    Returns dataset filtered by year.
+    """
+    return df.loc[df["FECHA"].str.contains(str(year))]
+
+
+def get_probability_of_numbers_per_column(df) -> dict:
+
+    columns = dict()
+
+    for column in df.columns:
+        columns[column] = dict(df[column].value_counts())
+
+    return columns
+
+
