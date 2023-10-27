68c539b44b08cbf46939b43cd449a936e2331116
ivanleoncz <ivanlmj@gmail.com>
2023-10-27 12:07:46 -0600

    feat: add --year option (filter dataframe by year)
    Also, improving argument parsing structure, better code and removing
    repeated instructions.

--- run.py
+++ run.py
@@ -3,26 +3,33 @@ import json
 
 import pandas as pd
 
-from utils import (prepare_dataframe_tris, prepare_dataframe_melate_retro, get_numbers_probability_per_column,
-                   get_numbers_probability)
+from utils import (prepare_dataframe_tris, prepare_dataframe_melate_retro, filter_dataframe_by_year,
+                   get_numbers_probability_per_column, get_numbers_probability)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process and analyze datasets from Mexico Lottery services.")
     parser.add_argument('--type', '-t', help="name of the lottery service", required=True,
                         choices=['tris', 'melate_retro'], )
+    parser.add_argument('--year', '-y', help="filter results by year",)
     parser.add_argument('--all_columns', '-a', help="perform calculation on all columns",
                         action='store_true')
     args = parser.parse_args()
     if args.type:
         if args.type == 'tris':
             df = prepare_dataframe_tris(pd.read_csv('datasets/Tris.csv'))
+            if args.year:
+                df = filter_dataframe_by_year(df, year=args.year)
             if args.all_columns:
-                print(json.dumps(get_numbers_probability(df), indent=2, default=int))
+                df = get_numbers_probability(df)
             else:
-                print(json.dumps(get_numbers_probability_per_column(df), indent=2, default=int))
+                df = get_numbers_probability_per_column(df)
+            print(json.dumps(df, indent=2, default=int))
         if args.type == 'melate_retro':
             df = prepare_dataframe_melate_retro(pd.read_csv('datasets/Melate-Retro.csv'))
+            if args.year:
+                df = filter_dataframe_by_year(df, year=args.year)
             if args.all_columns:
-                print(json.dumps(get_numbers_probability(df), indent=2, default=int))
+                df = get_numbers_probability(df)
             else:
-                print(json.dumps(get_numbers_probability_per_column(df), indent=2, default=int))
+                df = get_numbers_probability_per_column(df)
+            print(json.dumps(df, indent=2, default=int))
