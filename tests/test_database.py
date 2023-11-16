47339029c61bd813b6d8bc335093c566580c14fa
ivanleoncz <ivanlmj@gmail.com>
2023-11-15 20:02:50 -0600

    feat: database testing prototype
    Work in progress, but at least, it tests the creation and deletion (if
    necessary) of database tables.

---
+++ tests/test_database.py
@@ -0,0 +1,28 @@
+import os
+
+from database import Database
+
+
+class TestDatabase():
+
+    db = None
+    database = "tests.db"
+
+    @classmethod
+    def setup_class(cls):
+        cls.db = Database(database=cls.database)
+
+    @classmethod
+    def teardown_class(cls):
+        cls.db.drop_db()
+        os.remove(cls.database)
+
+    def test_init_db(self):
+        self.__class__.db.init_db()
+        result = self.__class__.db.cur.execute(""" SELECT name FROM sqlite_master; """)
+        assert len(result.fetchall()) == 3
+
+    def test_drop_db(self):
+        self.__class__.db.drop_db()
+        result = self.__class__.db.cur.execute(""" SELECT name FROM sqlite_master; """)
+        assert len(result.fetchall()) == 0
